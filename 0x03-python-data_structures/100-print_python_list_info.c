ff1597223bae60a38f19368d0c319872c89f0dc5
O-tero <oteronickbrian@gmail.com>
2022-06-06 09:40:46 +0000

    0: Python lists

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -17,7 +17,6 @@ void print_python_list_info(PyObject *p)
 	printf("[*] Size of the Python List = %ld\n", size);
 
 	list = (PyListObject *)p;
-
 	printf("[*] Allocated = %ld\n", list->allocated);
 
 	for (i = 0; i < size; i++)
