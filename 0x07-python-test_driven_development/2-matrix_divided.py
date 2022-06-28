150106c4bd9481e9992ad00a91e24d4f9a67325f
O-tero <oteronickbrian@gmail.com>
2022-06-28 13:07:21 +0000

    Python - Test-driven development

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,7 +8,6 @@ def matrix_divided(matrix, div):
     """Divide all elements of a matrix.
 
     Args:
-
         matrix (list): A list of lists of ints or floats.
         div (int/float): The divisor.
     Raises:
