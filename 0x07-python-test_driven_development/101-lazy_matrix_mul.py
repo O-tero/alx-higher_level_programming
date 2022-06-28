762c9da026341c628a70c2f12f7513aaa3091c3a
O-tero <oteronickbrian@gmail.com>
2022-06-28 21:55:26 +0000

    Python - Test-driven development

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -12,4 +12,5 @@ def lazy_matrix_mul(m_a, m_b):
         m_a (list of lists of ints/floats): The first matrix.
         m_b (list of lists of ints/floats): The second matrix.
     """
+
     return (np.matmul(m_a, m_b))
