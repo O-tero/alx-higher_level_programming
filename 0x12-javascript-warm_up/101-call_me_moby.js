b44220fc5129714622afc53c71583a533a9887d3
O-tero <oteronickbrian@gmail.com>
2022-09-05 13:50:33 +0300

    JavaScript - Warm up

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+module.exports = {
+    callMeMoby: function (n, f) {
+      for (let i = 0; i < n; i++) {
+        f();
+      }
+    }
+  };
+  
---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+module.exports = {
+    addMeMaybe: function (n, f) {
+      return f(n + 1);
+    }
+  };
+  
