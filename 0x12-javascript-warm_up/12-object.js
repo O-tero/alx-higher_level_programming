b9b5fefa8b9cc2b0a2491d1f5505e239f46a4ce3
O-tero <oteronickbrian@gmail.com>
2022-09-05 23:31:28 +0300

    JavaScript - Warm up

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-let myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
-  myObject.value = 89;
-  console.log(myObject);
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
   
